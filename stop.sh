f91d5cf3739aa57c346977f2b26cbb0614e68260
Pablo Smith <pabs159@gmail.com>
2024-03-17 12:10:24 -0500

    update readme syntax

--- stop.sh
+++ stop.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 #
 # Stop the running container 
+
+CWD=$(pwd)
+echo $CWD
 DC=$1
 IS_RUNNING="$(docker ps -aq -f status=running -f name=$DC)"
 if [ $IS_RUNNING ]; then
     echo "stopping $1"
+    docker cp ${DC}:/home/music-genre-classifer/GTZAN-Deep-Learning.ipynb ${CWD}/container_notebook.ipynb
     docker stop $DC
     exit
 fi
